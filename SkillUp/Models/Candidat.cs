ceaae91a118d085604f6e7a613d2272e2a9c2ade
Ahmed Ayadi <ayadi.ahmed4@gmail.com>
2023-05-06 22:18:11 +0100

    maj

--- SkillUp/Models/Candidat.cs
+++ SkillUp/Models/Candidat.cs
@@ -10,8 +10,8 @@ namespace SkillUp.Models
 		}
         public int Id { get; set; }
         public string Name { get; set; }
-		public int[] achatsId { get; set; }
-        public IEnumerable<Achat>? achats { get; set; }
+		//public int[] achatsId { get; set; }
+		public IEnumerable<Achat>? achats { get; set; } 
     }
 }
 
